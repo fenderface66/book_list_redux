78560a7231e4ab50d4d82fa28e0512c55a54a188
fenderface66 <r.heygate@gmail.com>
2017-06-07 19:41:17 +0100

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,20 @@
+export default function () {
+	return [
+		
+		{
+			title: 'Harry Potter 2'
+		},
+		{
+			title: 'Harry Potter 4'
+		},
+		{
+			title: 'Harry Potter 8'
+		},
+		{
+			title: 'Harry Potter 16'
+		},
+
+
+
+	]
+}
